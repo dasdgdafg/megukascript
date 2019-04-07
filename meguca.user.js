45b31faab3e6804928b2b70f32bead4dfff62822
dasdgdafg <dasdgdafg@cock.li>
2019-04-07 19:53:45 -0400

    Update version number

--- meguca.user.js
+++ meguca.user.js
@@ -9,7 +9,7 @@
 // @include     https://chiru.no/*
 // @connect     meguca.org
 // @connect     chiru.no
-// @version     3.7.0
+// @version     3.7.1
 // @author      medukasthegucas
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
